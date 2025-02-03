c0ca30f12623bdf13d2de4526a75f187740aa883
Vitalii <131532753+VITALA6@users.noreply.github.com>
2025-02-03 23:47:35 +0100

    Update main.cpp
    add comments

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include "funk.cpp" // Dołączenie pliku z funkcjami
 
 int main() {
-    system("chcp 65001 >> null"); // Obsługa polskich znaków
-    wczytajNazwyFunkcji("nazwyFunkcji.txt"); // Wczytanie nazw funkcji z pliku
+    system("chcp 65001 >> null"); // Ustawienie kodowania na UTF-8 dla obsługi polskich znaków
+    char wybranaFunkcja; // Zmienna przechowująca wybraną funkcję
 
-    char wybranaFunkcja;
-    while (SprawdzWybor(wybranaFunkcja)) { // Główna pętla programu
-        int wyborGranic = WyborWprowadzeniaGranic(); // Wybór sposobu wprowadzenia granic
-        double dolnaGranica, gornaGranica;
-        UstalGraniceCalkowania(wyborGranic, wybranaFunkcja, dolnaGranica, gornaGranica); // Ustalenie granic
-        ObliczIZaprezentujWynik(wybranaFunkcja, dolnaGranica, gornaGranica); // Obliczenie i prezentacja wyniku
+    // Wczytanie nazw funkcji z pliku "nazwyFunkcji.txt"
+    wczytajNazwyFunkcji("nazwyFunkcji.txt");
+
+    // Główna pętla programu
+    while (SprawdzWybor(wybranaFunkcja)) { // Sprawdzenie poprawności wyboru funkcji
+        int wyborGranic = WyborWprowadzeniaGranic(); // Wybór sposobu wprowadzenia granic całkowania
+        double dolnaGranica, gornaGranica; // Zmienne przechowujące granice całkowania
+
+        // Ustalenie granic całkowania na podstawie wyboru użytkownika
+        UstalGraniceCalkowania(wyborGranic, wybranaFunkcja, dolnaGranica, gornaGranica);
+
+        // Obliczenie i prezentacja wyniku całkowania
+        ObliczIZaprezentujWynik(wybranaFunkcja, dolnaGranica, gornaGranica);
     }
+
     return 0;
 }

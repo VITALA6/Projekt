2e522d7cd69f5bf14aef899b172409b3ae9ec4ce
Vitalii <131532753+VITALA6@users.noreply.github.com>
2025-05-27 17:10:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
-#include <cstdlib>   // Biblioteka do funkcji systemowych (np. system)
-#include "funk.cpp" // Dołączenie pliku z funkcjami
+#include <cstdlib>   
+#include "funk.cpp" 
 
 int main() {
-    system("chcp 65001 >> null"); // Ustawienie kodowania na UTF-8 dla obsługi polskich znaków
+    system("chcp 65001 >> null");
 
-    // Wczytanie nazw funkcji i granic całkowania z pliku
     wczytajNazwyFunkcji("nazwyFunkcji.txt");
     graniceZPliku("granice.txt");
 
-    char wybranaFunkcja; // Zmienna przechowująca wybraną funkcję
+    char wybranaFunkcja;
 
-    // Główna pętla programu
-    while (sprawdzWybor(wybranaFunkcja)) { // Sprawdzenie poprawności wyboru funkcji
-        double dolnaGranica, gornaGranica; // Zmienne przechowujące granice całkowania
-        int wyborGranic = wyborWprowadzeniaGranic(); // Wybór sposobu wprowadzenia granic całkowania
+    while (sprawdzWybor(wybranaFunkcja)) {
+        double dolnaGranica, gornaGranica;
+        int wyborGranic = wyborWprowadzeniaGranic();
        
-        // Ustalenie granic całkowania na podstawie wyboru użytkownika
         ustalGraniceCalkowania(wyborGranic, wybranaFunkcja, dolnaGranica, gornaGranica);
 
-        // Obliczenie i prezentacja wyniku całkowania
         obliczIZaprezentujWynik(wybranaFunkcja, dolnaGranica, gornaGranica);
     }
 
